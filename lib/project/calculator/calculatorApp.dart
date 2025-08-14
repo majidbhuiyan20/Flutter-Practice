4db2739428b7090d1f69db1a444a67ccf51a8e76
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-08-14 16:11:48 +0600

    make basic calculator app logic

--- lib/project/calculator/calculatorApp.dart
+++ lib/project/calculator/calculatorApp.dart
@@ -9,6 +9,59 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
+
+  String _output = '0';
+  String _input = '0';
+  String _operator = '0';
+  double num1 = 0;
+  double num2 = 0;
+
+void buttonPressed(String value){
+  print("Print Vlaue: $value");
+
+  setState(() {
+    if(value == 'C'){
+     _output = '0';
+     _input = '0';
+     _operator = '';
+     num1 = 0;
+     num2 = 0;
+    }
+    else if(value == '='){
+      num2 = double.parse(_input);
+      if(_operator == '+'){
+        _output = (num1 + num2).toString();
+      }
+      else if(_operator == '-'){
+        _output = (num1 - num2).toString();
+      }
+      else if(_operator == '*'){
+        _output = (num1 * num2).toString();
+      }
+      else if(_operator == '/'){
+        _output = num2 !=0 ? (num1 / num2).toString(): "Error";
+
+      }
+    }
+    else if(value == '+' || value == '-' || value == '*' || value == '/'){
+      num1 = double.parse(_input);
+      _operator = value;
+      _input = '';
+    }
+    else{
+      if(_input == '0'){
+        _input = value;
+      }
+      else
+        {
+          _input += value;
+        }
+      _output = _input;
+    }
+
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,40 +71,44 @@ class _CalculatorAppState extends State<CalculatorApp> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Container(
-              child: Text("0", style: TextStyle(fontSize: 50, color: Colors.white, fontWeight: FontWeight.bold),),
               alignment: Alignment.bottomRight,
               padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(_output, style: TextStyle(fontSize: 50, color: Colors.white, fontWeight: FontWeight.bold),),
+                ],
+              ),
             ),
             Row(
               children: [
-                buildCalculatorButton(onClick:(){}, text: '7', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '8', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '9', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '/', color: Colors.orange,),
+                buildCalculatorButton(onClick:()=> buttonPressed('7'), text: '7', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('8'), text: '8', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('9'), text: '9', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('/'), text: '/', color: Colors.orange,),
               ],
             ),
             Row(
               children: [
-                buildCalculatorButton(onClick:(){}, text: '4', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '5', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '6', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '*', color: Colors.orange,),
+                buildCalculatorButton(onClick:()=> buttonPressed('4'), text: '4', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('5'), text: '5', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('6'), text: '6', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('*'), text: '*', color: Colors.orange,),
               ],
             ),
             Row(
               children: [
-                buildCalculatorButton(onClick:(){}, text: '3', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '2', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '1', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '-', color: Colors.orange,),
+                buildCalculatorButton(onClick:()=> buttonPressed('3'), text: '3', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('2'), text: '2', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('1'), text: '1', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('-'), text: '-', color: Colors.orange,),
               ],
             ),
             Row(
               children: [
-                buildCalculatorButton(onClick:(){}, text: 'C', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '0', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '=', color: null,),
-                buildCalculatorButton(onClick:(){}, text: '+', color: Colors.orange,),
+                buildCalculatorButton(onClick:()=> buttonPressed('C'), text: 'C', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('0'), text: '0', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('='), text: '=', color: null,),
+                buildCalculatorButton(onClick:()=> buttonPressed('+'), text: '+', color: Colors.orange,),
               ],
             )
             ],

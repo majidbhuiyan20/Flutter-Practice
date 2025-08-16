8a9c0d040942bccf2141487bc789dc308405c25c
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-08-17 03:40:01 +0600

    money management app functional

--- lib/project/money_management/money_management.dart
+++ lib/project/money_management/money_management.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class MoneyManagement extends StatefulWidget {
   const MoneyManagement({super.key});
@@ -11,6 +12,13 @@ class _MoneyManagementState extends State<MoneyManagement>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
+  List<Map<String, dynamic>> _earning = [];
+  List<Map<String, dynamic>> _expense = [];
+
+  double get totalExpanse => _expense.fold(0, (sum, item) => sum + item['amount']);
+  double get totalEarning => _earning.fold(0, (sum, item) => sum + item['amount']);
+  double get balance => totalEarning - totalExpanse;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -21,7 +29,7 @@ class _MoneyManagementState extends State<MoneyManagement>
   void _showFABOptions(BuildContext context){
       showModalBottomSheet(context: context, builder: (context){
         return Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(12.0),
           child: Row(
             children: [
                 Expanded(
@@ -29,7 +37,10 @@ class _MoneyManagementState extends State<MoneyManagement>
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.green,
                       ),
-                      onPressed: (){}, child: Text("Add Income", style: TextStyle(color: Colors.white),)),
+                      onPressed: (){
+                        Navigator.pop(context);
+                        _showForm(isEarning: true);
+                      }, child: Text("Add Income", style: TextStyle(color: Colors.white),)),
                 ),
                 SizedBox(width: 10,),
                 Expanded(
@@ -37,12 +48,122 @@ class _MoneyManagementState extends State<MoneyManagement>
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.red,
                       ),
-                      onPressed: (){}, child: Text("Add Expense", style: TextStyle(color: Colors.white),)),
+                      onPressed: (){
+                        Navigator.pop(context);
+                        _showForm(isEarning: false);
+                      }, child: Text("Add Expense", style: TextStyle(color: Colors.white),)),
                 ),
               ],
           ),
         );
       });
+  }
+
+  void _showForm({required bool isEarning}){
+    TextEditingController titleController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+    DateTime entryDate = DateTime.now();
+
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true, // Allows the bottom sheet to take full screen height if needed
+        builder: (context){
+      return Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom, // Adjust padding when keyboard appears
+          left: 16.0,
+          right: 16.0,
+          top: 16.0,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+                Text(
+                  isEarning ? "Add Income" : "Add Expense",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: isEarning ? Colors.green : Colors.red),
+                ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Title',
+                hintText: 'Enter title',
+                prefixIcon: Icon(Icons.title),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: BorderSide(color: isEarning ? Colors.green : Colors.red, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 16,),
+            TextField(
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Amount',
+                hintText: 'Enter amount',
+                prefixIcon: Icon(Icons.attach_money),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: BorderSide(color: isEarning ? Colors.green : Colors.red, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 12,),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: isEarning ? Colors.green : Colors.red,
+
+                  ),
+                  onPressed: (){
+                    if(titleController.text.isNotEmpty && amountController.text.isNotEmpty){
+                    _addEntry(titleController.text, double.parse(amountController.text), entryDate, isEarning);
+                    Navigator.pop(context);
+                    }
+                  }, child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                                  isEarning ? "Add Income" : "Add Expense", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w600),
+                                ),
+                  )),
+            ),
+            SizedBox(height: 20,),
+          ],
+        ),
+      );
+    });
+
+  }
+
+  void _addEntry(String title, double amount, DateTime date, bool isEarning){
+
+    if(isEarning){
+      setState(() {
+        _earning.add({
+          'title': title,
+          'amount': amount,
+          'date': date,
+        });
+      });
+    }
+    else{
+      setState(() {
+        _expense.add({
+          'title': title,
+          'amount': amount,
+          'date': date,
+        });
+      });
+    }
+
   }
 
   @override
@@ -71,6 +192,7 @@ class _MoneyManagementState extends State<MoneyManagement>
               child: Text("Expense", style: TextStyle(color: Colors.white)),
             ),
           ],
+
         ),
       ),
       body: Column(
@@ -80,20 +202,28 @@ class _MoneyManagementState extends State<MoneyManagement>
             children: [
               _buildSummaryCard(
                 title: 'Earning',
-                value: 25000,
+                value: totalEarning,
                 color: Colors.green,
               ),
               _buildSummaryCard(
                 title: 'Expense',
-                value: 10000,
+                value: totalExpanse,
                 color: Colors.red,
               ),
               _buildSummaryCard(
                 title: 'Balance',
-                value: 15000,
+                value: balance,
                 color: Colors.blue,
               ),
             ],
+          ),
+          Expanded(
+            child: TabBarView(
+                controller: _tabController,
+                children: [
+              _buildList(_earning, Colors.green, true),
+              _buildList(_expense, Colors.red, false),
+            ]),
           ),
         ],
       ),
@@ -141,4 +271,24 @@ Widget _buildSummaryCard({
       ),
     ),
   );
+}
+
+
+Widget _buildList(List<Map<String, dynamic>> items, Color color, bool isEarning){
+  return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index){
+    return Card(
+      child: ListTile(
+        leading: CircleAvatar(
+ backgroundColor: color.withOpacity(0.2),
+          child: Icon(isEarning ? Icons.arrow_upward : Icons.arrow_downward, color: color,),
+ ),
+        title: Text(items[index]['title']),
+        subtitle: Text(DateFormat('dd/MM/yyyy HH:mm').format(items[index]['date'])),
+        trailing: Text('${isEarning ? '+' : '-'} ${items[index]['amount'].toString()}', style: TextStyle(color:color, fontSize: 16, fontWeight: FontWeight.bold),),
+
+        ),
+      );
+  });
 }

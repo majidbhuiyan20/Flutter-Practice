6664d54491151351ecf02ebad63e077c1f095f77
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-08-20 04:32:50 +0600

    widget extraction product list

--- lib/project/crud_app/crud_app_screen.dart
+++ lib/project/crud_app/crud_app_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practice/project/crud_app/add_new_product_screen.dart';
+import 'package:practice/project/crud_app/product_item.dart';
 import 'package:practice/project/crud_app/update_product_screen.dart';
 
 class CrudAppScreen extends StatefulWidget {
@@ -24,54 +25,7 @@ class _CrudAppScreenState extends State<CrudAppScreen> {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: ListView.separated(
-          itemCount: 10,
-          itemBuilder: (context, index){
-        return ListTile(
-          leading: CircleAvatar(),
-          title: const Text("Product Name"),
-          subtitle:  Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Code: 65dghs65"),
-              Row(
-                spacing: 16,
-                children: [
-                  Text("Quantity"),
-                  Text("Unit Price"),
-                ],
-
-              ),
-            ],
-            
-          ),
-          trailing: PopupMenuButton<productOptions>(itemBuilder: (context){
-            return [PopupMenuItem(
-              value: productOptions.update,
-              child: Text("Update"),),
-            PopupMenuItem(
-              value: productOptions.delete,
-              child: Text("Delete"),)];
-          },
-            onSelected: (productOptions selectedOption){
-            if(selectedOption == productOptions.delete){
-                print("delete");
-            }
-            else if(selectedOption == productOptions.update){
-                print("Update");
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> UpdateProductScreen()));
-            }
-            },
-
-          ),
-        );
-      },
-        separatorBuilder: (context, index) {
-            return Divider(
-              indent: 70,
-            );
-        }
-      ),
+      body: product_item(),
       floatingActionButton: FloatingActionButton(onPressed: (){
         Navigator.push(context, MaterialPageRoute(builder: (context)=> AddNewProductScreen()));
         },
@@ -82,9 +36,4 @@ class _CrudAppScreenState extends State<CrudAppScreen> {
     );
   }
 }
-
-
-enum productOptions{
-  update,
-  delete,
 }
---
+++ lib/project/crud_app/product_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+class product_item extends StatelessWidget {
+  const product_item({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        itemCount: 10,
+        itemBuilder: (context, index){
+          return ListTile(
+            leading: CircleAvatar(),
+            title: const Text("Product Name"),
+            subtitle:  Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Code: 65dghs65"),
+                Row(
+                  spacing: 16,
+                  children: [
+                    Text("Quantity"),
+                    Text("Unit Price"),
+                  ],
+
+                ),
+              ],
+
+            ),
+            trailing: PopupMenuButton<productOptions>(itemBuilder: (context){
+              return [PopupMenuItem(
+                value: productOptions.update,
+                child: Text("Update"),),
+                PopupMenuItem(
+                  value: productOptions.delete,
+                  child: Text("Delete"),)];
+            },
+              onSelected: (productOptions selectedOption){
+                if(selectedOption == productOptions.delete){
+                  print("delete");
+                }
+                else if(selectedOption == productOptions.update){
+                  print("Update");
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> UpdateProductScreen()));
+                }
+              },
+
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(
+            indent: 70,
+          );
+        }
+    );
+  }
+}
+
+
+enum productOptions{
+  update,
+  delete,

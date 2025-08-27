c5d47a423f243efbace0cb9f4d4c9c52163d258f
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-08-28 02:02:34 +0600

    create product model

---
+++ lib/project/crud_app/models/products.dart
@@ -0,0 +1,16 @@
+// "_id": "68af6129dd287e3f18a26813",
+// "ProductName": "Kola",
+// "ProductCode": 83468236,
+// "Img": "kola.com",
+// "Qty": 80,
+// "UnitPrice": 8,
+// "TotalPrice": 640
+class Product{
+  late String id;
+  late String name;
+  late String code;
+  late String image;
+  late int quantity;
+  late int unitPrice;
+  late int totalPrice;
+}

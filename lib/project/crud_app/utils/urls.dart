237a20e0aa1d3dbfeb82684ab9179776581b7298
majidbhuiyan20 <65237569+majidbhuiyan20@users.noreply.github.com>
2025-08-29 01:13:32 +0600

    create custom url

---
+++ lib/project/crud_app/utils/urls.dart
@@ -0,0 +1,6 @@
+class Urls{
+  static const String baseUrl = 'http://35.73.30.144:2008/api/v1';
+  static const String createProduct = '$baseUrl/CreateProduct';
+  static const String getProductUrl = '$baseUrl/ReadProduct';
+
+}

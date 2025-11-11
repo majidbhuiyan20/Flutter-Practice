3700dfc0992a415391b499dfaffc74105948b7de
Majid Bhuiyan <65237569+majidbhuiyan20@users.noreply.github.com>
2025-11-11 23:53:07 +0600

    custom app bar

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatefulWidget implements PreferredSizeWidget {
+  final String title;
+  final Color backgroundColor;
+
+  const CustomAppbar({
+    super.key,
+    required this.title,
+    required this.backgroundColor,
+  });
+
+  @override
+  State<CustomAppbar> createState() => _CustomAppbarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _CustomAppbarState extends State<CustomAppbar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(widget.title),
+      backgroundColor: widget.backgroundColor,
+      centerTitle: true,
+    );
+  }
+}

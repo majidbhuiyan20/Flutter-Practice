e46de4d212871796696b17c7c82a2a4ddd6a2cce
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-02-17 00:24:45 +0600

    feat(ui): init recipe app

--- lib/recipe_app/features/home/view/recipe_home_screen.dart
+++ lib/recipe_app/features/home/view/recipe_home_screen.dart
@@ -10,6 +10,30 @@ class RecipeHomeScreen extends StatefulWidget {
 class _RecipeHomeScreenState extends State<RecipeHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0XFF038A46),
+        elevation: 0,
+        title: Text(
+          "Recipe App",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.favorite_border, color: Colors.white,),
+            onPressed: () {
+              // TODO: Open favorites
+            },
+          ),
+        ],
+      )
+      ,
+    );
   }
 }

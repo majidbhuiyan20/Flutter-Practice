713eec30acf2a968235d3723e87a4ddef1d6d519
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-02-17 01:02:13 +0600

    feat(api): create api client and recipe model

---
+++ lib/recipe_app/features/network/api_client.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  final Dio dio;
+  ApiClient(this.dio){
+    dio.options = BaseOptions(
+      baseUrl: "https://dummyjson.com",
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 20)
+    );
+  }
+  Future<Response> get(String path) async {
+    return await dio.get(path);
+  }
+
+}

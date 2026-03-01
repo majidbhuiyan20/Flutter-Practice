22626299fbac61271b7ca91c9383619dad5a9d80
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-03-01 14:54:11 +0600

    init stripe

--- android/app/src/main/kotlin/com/example/practice/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/practice/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.practice
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ lib/stripe/view/stripe_payment_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StripePaymentScreen extends StatefulWidget {
+  const StripePaymentScreen({super.key});
+
+  @override
+  State<StripePaymentScreen> createState() => _StripePaymentScreenState();
+}
+
+class _StripePaymentScreenState extends State<StripePaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
